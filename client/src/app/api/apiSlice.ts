a002dc49a49e1e58cbce4f3ea28ecdf5dca9ec1b
Anton Podkur <59440452+antonpodkur@users.noreply.github.com>
2023-05-22 23:58:24 +0300

    Added modals for saving and deleting routes

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -1,37 +1,36 @@
 import { BaseQueryApi, FetchArgs, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { setUser, logOut, setLoggedIn } from '../../features/auth/authSlice'
+import { logOut, setLoggedIn } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:4000',
-    credentials: 'include',
+  baseUrl: 'http://localhost:4000',
+  credentials: 'include',
 })
 
-//TODO: Save access token to localstorage and verify if it exists
 
-const baseQueryWithReauth = async ( args: string | FetchArgs, api: BaseQueryApi, extraOptions: any )  => {
-    let result = await baseQuery(args, api, extraOptions)
+const baseQueryWithReauth = async (args: string | FetchArgs, api: BaseQueryApi, extraOptions: any) => {
+  let result = await baseQuery(args, api, extraOptions)
 
-    if (result?.error?.status === 401) {
-        console.log("refreshing access token")
-        const refreshResult = await baseQuery('/api/v1/auth/refresh', api, extraOptions);
-        console.log(refreshResult)
-        if (refreshResult?.data) {
-            if (refreshResult.data.status === 'success') {
-                api.dispatch(setLoggedIn({}))
-                result = await baseQuery(args, api, extraOptions);
-            }
-            else {
-                api.dispatch(logOut({}));
-            }
-        }
-        else {
-            api.dispatch(logOut({}));
-        }
+  if (result?.error?.status === 401) {
+    console.log("refreshing access token")
+    const refreshResult = await baseQuery('/api/v1/auth/refresh', api, extraOptions);
+    console.log(refreshResult)
+    if (refreshResult?.data) {
+      if (refreshResult.data.status === 'success') {
+        api.dispatch(setLoggedIn({}))
+        result = await baseQuery(args, api, extraOptions);
+      }
+      else {
+        api.dispatch(logOut({}));
+      }
     }
-    return result
+    else {
+      api.dispatch(logOut({}));
+    }
+  }
+  return result
 }
 
 export const apiSlice = createApi({
-    baseQuery: baseQueryWithReauth,
-    endpoints: builder => ({ }) 
+  baseQuery: baseQueryWithReauth,
+  endpoints: builder => ({})
 })
--- client/src/features/auth/authSlice.ts
+++ client/src/features/auth/authSlice.ts
@@ -3,35 +3,36 @@ import { User } from '../../models/user';
 import { RootState } from '../../app/store';
 
 interface AuthState {
-    user: User | null;
-    isLoggedIn: boolean
-  }
-  
+  user: User | null;
+  isLoggedIn: boolean
+}
+
 const initialState: AuthState = {
-    user: null,
-    isLoggedIn: false
+  user: null,
+  isLoggedIn: false
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        setUser: (state, action: PayloadAction<User>) => {
-            state.user = action.payload
-        },
-        setLoggedIn: (state, action) => {
-            state.isLoggedIn = true
-        },
-        logOut: (state, action) => {
-            state.user = null
-            state.isLoggedIn = false
-        }
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload
     },
+    setLoggedIn: (state, action) => {
+      state.isLoggedIn = true
+    },
+    logOut: (state, action) => {
+      state.user = null
+      state.isLoggedIn = false
+    }
+  },
 });
 
-export const { setUser, setLoggedIn ,logOut } = authSlice.actions
+export const { setUser, setLoggedIn, logOut } = authSlice.actions
 
 export default authSlice.reducer
 
 export const selectCurrentUser = (state: RootState) => state.auth.user;
 export const selectIsLoggedIn = (state: RootState) => state.auth.isLoggedIn;
+export const selectUser = (state: RootState) => state.auth.user;
